d19d49197a6652a471e4bf34e15c97e48fb7586f
Curtis F Kaylor <revcurtis@gmail.com>
2021-03-06 23:54:38 -0500

    makeasm.py: Convert RST operands for TASM, option to create label listing

--- disassembly/makeasm.py
+++ disassembly/makeasm.py
@@ -1,6 +1,11 @@
-# Convert Annotated Aquarius ROM Disassembly to TASM Source Code
+# Mattel Aquarius ROM Disassembly Convesions
 
-def convert(iname, oname):
+# Convert Annotated Aquarius ROM Disassembly to TASM Source Code
+def makeasm(iname, oname):
+
+  #RST argument substitution dictionary
+  RST = {"START": "00H", "SYNCHK": "08H", "CHRGET": "10H", "OUTCHR": "18H",
+         "COMPAR": "20H", "FSIGN": "28H", "HOOKDO": "30H", "USRFN": "38H"}
   
   ifile = open(iname, 'r')
   ofile = open(oname, 'w')
@@ -9,7 +14,11 @@ def convert(iname, oname):
   llmeta = False
   llblock = False
   
+  lineno = 0
+  
   for line in ifile:
+    lineno += 1
+  
     line = line.rstrip()
     blank = False if len(line) else True     
 
@@ -38,6 +47,14 @@ def convert(iname, oname):
     #Remove Address and Object Code Prefix
     line = line[24:]
 
+    #Replace RST operand for TASM
+    if line[0:1] != ';' and line[8:16] == "rst     ":
+      arg = line[16:24].rstrip()
+      if arg in RST:
+        line = line[:16] + RST[arg].ljust(8) + line[24:]
+      else:
+        print("Illegal RST operand %s in line %d" % (arg, lineno))
+
     #Write Line to ASM File
     ofile.write(line + '\n')
 
@@ -45,6 +62,33 @@ def convert(iname, oname):
     
   ofile.close()
   ifile.close()
-  
-convert("aquarius-rom.lst", "s2basic.asm")
-  
+
+# Convert Annotated Aquarius ROM Disassembly to TASM Source Code
+def makesym(iname, oname):
+  ifile = open(iname, 'r')
+  ofile = open(oname, 'w')
+
+  for line in ifile:
+    line = line.rstrip()
+    if len(line) == 0:      continue  #Ignore Blank Lines
+    if line[0:] == ';':     continue  #Ignore Meta Comments
+    if line[0:] == ':':     continue  #Ignore Temporary Labels
+    if line[24:25] == ';':  continue  #Ignore Block Comments
+    if line[18:19] == ';':  continue  #Ignore Unreferenced Labels
+    if line[18:19] == ';':  continue  #Ignore Local Labels
+
+    address = line[0:4]  #Extract Address
+    if address == "    ":   continue  #No Address Found
+    
+    symbol = line[24:32].rstrip()     #Extract Symbol from Line
+    if len(symbol) == 0:    continue  #No Symbol Found  
+    if symbol[-1:] != ':':  continue  #Not a label
+    label = symbol[:-1].ljust(8)      #Remove Colon from End 
+    
+    ofile.write(address + '    ' + label + '\n')
+
+  ofile.close()
+  ifile.close()
+    
+makeasm("aquarius-rom.lst", "s2basic.asm")  
+#makesym("aquarius-rom.lst", "aquarius-rom.sym")
